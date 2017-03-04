a7bd397b26d3e45d0867d156dbc09416e6a820ac
Jesse Poteet <jpoteet6@gmail.com>
2017-03-03 23:38:45 -0700

    mailgun fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,14 +84,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # config.action_mailer.default_url_options = { host: 'https://immense-ocean-48880.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
 
-ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'immense-ocean-48880.heroku.com',
-  :authentication => :plain,
-}
-ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'immense-ocean-48880.heroku.com',
+    :authentication => :plain,
+  }
 end
