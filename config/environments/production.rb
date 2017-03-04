d5fba9e306a0c7d838e9e6e092a346bafe2ffba4
Jesse Poteet <jpoteet6@gmail.com>
2017-03-03 23:28:26 -0700

    fixed mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.mailgun.org',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['MAILGUN_USERNAME'],
-  :password       => ENV['MAILGUN_PASSWORD'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
